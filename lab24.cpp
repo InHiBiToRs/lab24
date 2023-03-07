d0247fdd1a9eeecae75f02227016751a7b83249a
InHiBiToRs <tanakrit64566@gmail.com>
2023-03-07 17:03:54 +0700

    lab24_complete

--- lab24.cpp
+++ lab24.cpp
@@ -24,10 +24,87 @@ ComplexNumber ComplexNumber::operator+(const ComplexNumber &c){
 	return ComplexNumber(real+c.real,imag+c.imag);
 }
 
+ComplexNumber operator+(double s, const ComplexNumber &c){
+	return ComplexNumber(s+c.real,c.imag);
+}
+
 ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
 }
 
+ComplexNumber operator-(double s, const ComplexNumber &c){
+	return ComplexNumber(s-c.real,-c.imag);
+}
+
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber(real*c.real-imag*c.imag,imag*c.real+real*c.imag);
+}
+
+ComplexNumber operator*(double s, const ComplexNumber &c){
+	return ComplexNumber(s*c.real,s*c.imag);
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	return ComplexNumber((real*c.real+imag*c.imag)/(pow(c.real,2)+pow(c.imag,2)),(imag*c.real-real*c.imag)/(pow(c.real,2)+pow(c.imag,2)));
+}
+
+ComplexNumber operator/(double s, const ComplexNumber &c){
+	return ComplexNumber((s*c.real)/(pow(c.real,2)+pow(c.imag,2)),(-s*c.imag)/(pow(c.real,2)+pow(c.imag,2)));
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if(real == c.real && imag == c.imag){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+bool operator==(double s, const ComplexNumber &c){
+	if(s == c.real && s == c.imag){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+double ComplexNumber::abs(){
+	return sqrt(pow(real,2)+pow(imag,2));
+}
+
+double ComplexNumber::angle(){
+	if(real < 0 && imag < 0){
+		return -180+atan(imag/real)*(180/M_PI);
+	}else if(real < 0){
+		return 180+atan(imag/real)*(180/M_PI);
+	}else{
+		return atan(imag/real)*(180/M_PI);
+	}
+}
+
+ostream & operator<<(ostream &os, const ComplexNumber &c){
+	if(c.real > 0 && c.imag > 0){
+		return os << c.real << "+" << c.imag << "i";
+	}else if(c.real == 0 && c.imag > 0){
+		return os << c.imag << "i";
+	}else if(c.real > 0 && c.imag == 0){
+		return os << c.real;
+	}else if(c.real == 0 && c.imag > 0){
+		return os << c.imag << "i";
+	}else if(c.real == 0 && c.imag == 0){
+		return os << c.real;
+	}else if(c.real > 0 && c.imag < 0){
+		return os << c.real << c.imag << "i";
+	}else if(c.real == 0 && c.imag < 0){
+		return os << c.imag << "i";
+	}else if(c.real < 0 && c.imag > 0){
+		return os << c.real << "+" << c.imag << "i";;
+	}else if(c.real < 0 && c.imag == 0){
+		return os << c.real;
+	}else{
+		return os << c.real << c.imag << "i";
+	}
+}
 //Write your code here
 
 int main(){
